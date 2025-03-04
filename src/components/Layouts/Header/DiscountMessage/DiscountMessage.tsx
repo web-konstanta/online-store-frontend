3117c628a39a02b832dbb1f33d48fccf2f777695
Kostyantin <109417150+web-konstanta@users.noreply.github.com>
2025-03-04 22:51:06 +0200

    Create landing header (#2)
    * Create CSS landing setup
    
    * Create header (without the mobile adaptive)
    
    * Create adaptive CSS for the header v1.0

---
+++ src/components/Layouts/Header/DiscountMessage/DiscountMessage.tsx
@@ -0,0 +1,35 @@
+import { useRef } from 'react'
+import classes from './DiscountMessage.module.css'
+
+const DiscountMessage = () => {
+    const discountRef = useRef<HTMLDivElement | null>(null)
+
+    const closeDiscount = () => {
+        if (discountRef.current) {
+            discountRef.current.style.display = 'none'
+        }
+    }
+
+    return (
+        <div className={classes.header__discount}>
+            <div className={classes.header__discountContent} ref={discountRef}>
+                <div className={classes.header__discountEmpty}></div>
+                <p className={classes.header__discountMessage}>
+                    Sign up and get 20% off to your first order.{' '}
+                    <span>Sign Up Now</span>
+                </p>
+                <div
+                    className={classes.header__closeDiscount}
+                    onClick={() => closeDiscount()}
+                >
+                    <img
+                        src="/images/close-discount-message.png"
+                        alt="close discount message"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DiscountMessage
