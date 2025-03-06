14fbc5338e960967c120f802d31c38159c22772d
Makarenko Kostya <kochanmujik@gmail.com>
2025-03-07 00:26:20 +0200

    Add event for escape button

--- src/components/Layouts/Header/Options/Options.tsx
+++ src/components/Layouts/Header/Options/Options.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import BurgerMenu from '../../UI/BurgerMenu/BurgerMenu'
 import classes from './Options.module.css'
 
@@ -11,20 +11,48 @@ const Options = () => {
         setIsBurgerOpen((prev) => !prev)
     }
 
+    const showBurgerMenu = () => {
+        if (showBurgerRef.current) {
+            showBurgerRef.current.style.display = 'none'
+        }
+        if (hideBurgerRef.current) {
+            hideBurgerRef.current.style.display = 'block'
+        }
+        toggleBurgerMenu()
+    }
+
+    const hideBurgerMenu = () => {
+        if (showBurgerRef.current) {
+            showBurgerRef.current.style.display = 'block'
+        }
+        if (hideBurgerRef.current) {
+            hideBurgerRef.current.style.display = 'none'
+        }
+        toggleBurgerMenu()
+    }
+
+    useEffect(() => {
+        const handleEscape = (event: KeyboardEvent) => {
+            if (event.key === 'Escape') {
+                if (hideBurgerRef.current?.style.display === 'block') {
+                    hideBurgerMenu()
+                }
+            }
+        }
+
+        document.addEventListener('keydown', handleEscape)
+
+        return () => {
+            document.removeEventListener('keydown', handleEscape)
+        }
+    }, [])
+
     return (
         <div className={classes.header__options}>
             <div
                 ref={showBurgerRef}
                 className={classes.header__burgerMenu}
-                onClick={() => {
-                    if (showBurgerRef.current) {
-                        showBurgerRef.current.style.display = 'none'
-                    }
-                    if (hideBurgerRef.current) {
-                        hideBurgerRef.current.style.display = 'block'
-                    }
-                    toggleBurgerMenu()
-                }}
+                onClick={showBurgerMenu}
             >
                 <img src="/images/burger-menu.png" />
             </div>
@@ -33,15 +61,7 @@ const Options = () => {
                 className={`${classes.header__closeBurgerMenu} ${
                     isBurgerOpen ? classes.visible : ''
                 }`}
-                onClick={() => {
-                    if (showBurgerRef.current) {
-                        showBurgerRef.current.style.display = 'block'
-                    }
-                    if (hideBurgerRef.current) {
-                        hideBurgerRef.current.style.display = 'none'
-                    }
-                    toggleBurgerMenu()
-                }}
+                onClick={hideBurgerMenu}
             >
                 <img src="/images/close.png" />
             </div>
