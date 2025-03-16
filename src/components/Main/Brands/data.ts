a8c20b3bb7cc0caaf64e5372ccec5b1cb87f3c42
Kostyantin <109417150+web-konstanta@users.noreply.github.com>
2025-03-16 02:37:33 +0200

    Feature/main page brands (#10)
    * Create selecting brands icons
    
    * Add margin from the first block

---
+++ src/components/Main/Brands/data.ts
@@ -0,0 +1,22 @@
+export const brandsIcons: Array<{ path: string, href: string }> = [
+	{
+		path: '/images/main/brands/versace.png',
+		href: '#'
+	},
+	{
+		path: '/images/main/brands/zara.png',
+		href: '#'
+	},
+	{
+		path: '/images/main/brands/gucci.png',
+		href: '#'
+	},
+	{
+		path: '/images/main/brands/prada.png',
+		href: '#'
+	},
+	{
+		path: '/images/main/brands/calvin-klein.png',
+		href: '#'
+	}
+]
