6e80c52f9d983b4e4696b483fe0099fe0e1de402
Kostyantin Makarenko <kochanmujik@gmail.com>
2025-03-27 01:14:12 +0200

    Create type for review model & selected mock data

--- src/components/Main/Review/Item/ReviewItem.tsx
+++ src/components/Main/Review/Item/ReviewItem.tsx
@@ -1,19 +1,18 @@
+import { Review } from '../../../../types'
 import classes from './ReviewItem.module.css'
 
-const ReviewItem = () => {
+const ReviewItem = ({ review }: { review: Review }) => {
     return (
         <div className={classes.review__item}>
             <div className={classes.review__itemUser}>
-                <div className={classes.review__itemTitle}>Sarah M.</div>
+                <div className={classes.review__itemTitle}>
+                    {review.user_name}
+                </div>
                 <div>
                     <img src="/images/main/review/checked-user.png" />
                 </div>
             </div>
-            <div className={classes.review__itemText}>
-                "I'm blown away by the quality and style of the clothes I
-                received from Shop.co. From casual wear to elegant dresses,
-                every piece I've bought has exceeded my expectations.”
-            </div>
+            <div className={classes.review__itemText}>{review.content}</div>
         </div>
     )
 }
--- src/components/Main/Review/List/ReviewList.tsx
+++ src/components/Main/Review/List/ReviewList.tsx
@@ -1,8 +1,9 @@
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
 import ReviewItem from '../Item/ReviewItem'
 import classes from './ReviewList.module.css'
+import { Review } from '../../../../types'
 
 const ReviewList = () => {
     const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [
@@ -17,7 +18,43 @@ const ReviewList = () => {
         if (emblaApi) emblaApi.scrollNext()
     }, [emblaApi])
 
-    const reviews = Array(5).fill(0)
+    const [reviews] = useState<Review[]>([
+        {
+            id: 1,
+            user_name: 'Sarah M.',
+            content:
+                'Im blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece Ive bought has exceeded my expectations.',
+            rating: 4,
+        },
+        {
+            id: 2,
+            user_name: 'Alex K.',
+            content:
+                'Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.',
+            rating: 3,
+        },
+        {
+            id: 3,
+            user_name: 'James L.',
+            content:
+                'As someone whos always on the lookout for unique fashion pieces, Im thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.',
+            rating: 5,
+        },
+        {
+            id: 4,
+            user_name: 'Sarah M.',
+            content:
+                'Im blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece Ive bought has exceeded my expectations.',
+            rating: 3,
+        },
+        {
+            id: 5,
+            user_name: 'Alex K.',
+            content:
+                'Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.',
+            rating: 2,
+        },
+    ])
 
     return (
         <div className={classes.review__list}>
@@ -43,9 +80,9 @@ const ReviewList = () => {
                 </div>
                 <div className="embla__viewport" ref={emblaRef}>
                     <div className="embla__container">
-                        {reviews.map((_, index) => (
+                        {reviews.map((review, index) => (
                             <div key={index} className="embla__slide">
-                                <ReviewItem />
+                                <ReviewItem review={review} />
                             </div>
                         ))}
                     </div>
