c20610ac91a12e22c922107ae5700fdfdf3db490
Kostyantin Makarenko <kochanmujik@gmail.com>
2025-03-28 23:00:05 +0200

    Add stars rating selecting

--- src/components/Main/Product/Item/ProductItem.tsx
+++ src/components/Main/Product/Item/ProductItem.tsx
@@ -1,6 +1,7 @@
 import { Product } from '../../../../types'
 import classes from './ProductItem.module.css'
 import { productDiscount } from '../../../../utils/common/product'
+import Rating from '@mui/material/Rating'
 
 const ProductItem = ({ product }: { product: Product }) => {
     return (
@@ -9,8 +10,16 @@ const ProductItem = ({ product }: { product: Product }) => {
                 <img src={product.image} />
             </div>
             <div className={classes.product__itemTitle}>{product.name}</div>
-            <div className={classes.product__itemRating}>
-                <span>{product.rating}</span>/5
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Rating
+                    name="read-only"
+                    defaultValue={product.rating}
+                    precision={0.5}
+                    readOnly
+                />
+                <div className={classes.product__itemRating}>
+                    <span>{product.rating}</span>/5
+                </div>
             </div>
             <div className={classes.product__itemPrices}>
                 <div className={classes.product__itemNewPrice}>
