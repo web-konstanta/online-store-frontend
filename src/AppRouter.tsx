f6cffdf4077671fb75b6e2cf0c2c2dc71aae2f44
Kostyantin Makarenko <kochanmujik@gmail.com>
2025-04-04 01:36:19 +0300

    Fix scrolling pages after navigating by links

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,12 +2,14 @@ import Footer from './components/Layouts/Footer/Footer'
 import Header from './components/Layouts/Header/Header'
 import { publicRoutes } from './routes/publicRoutes'
 import { Routes, Route } from 'react-router-dom'
+import ScrollToTop from './components/Layouts/ScrollToTop/ScrollToTop'
 
 const AppRouter = () => {
     return (
         <div className="content">
             <Header />
             <div style={{ flex: 1 }}>
+                <ScrollToTop />
                 <Routes>
                     {publicRoutes.map((route, key) => (
                         <Route
