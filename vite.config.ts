a923795c08c88617c7bf583c8b39d4452ba59862
Myroslav Toloshnyi <dfgshte@gmail.com>
2025-04-03 22:29:26 +0300

    Add @src path aliases fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  },
-  resolve: {
-    alias: {
-      '@': './src'
-    }
-  }
-});
+    plugins: [react()],
+    server: {
+        port: 3000,
+    },
+    resolve: {
+        alias: {
+            '@src': resolve(__dirname, 'src'),
+        },
+    },
+})
