b06fc280a33a7f6b621d803e1ca3cf93fb81a460
Kostyantin Makarenko <kochanmujik@gmail.com>
2025-04-23 23:49:34 +0300

    Setup redis client & prisma client

---
+++ backend/src/lib/redis/client.ts
@@ -0,0 +1,8 @@
+import Redis from 'ioredis';
+
+const redis = new Redis({
+	host: process.env.REDIS_HOST,
+	port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT) : 6379
+});
+
+export default redis;
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
-async function bootstrap() {
+async function app() {
   const app = await NestFactory.create(AppModule)
   await app.listen(process.env.PORT ?? 5000)
 }
-bootstrap()
+
+app()
