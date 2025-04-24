8e4925993b7e7927bb45cd3c1d945e37b6a4e8c4
Kostyantin Makarenko <kochanmujik@gmail.com>
2025-04-25 01:12:15 +0300

    Create basic CORS and global validation setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { ValidationPipe } from '@nestjs/common'
 
 async function app() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 5000)
+
+  const config = app.get(ConfigService)
+
+  app.enableCors({
+    origin: config.get<string>('CLIENT_URL'),
+    methods: '*',
+    allowedHeaders: '*',
+    credentials: true
+  })
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true
+  }))
+
+  await app.listen(parseInt(config.get<string>('PORT')!) ?? 5000)
 }
 
 app()
